6c8c66dd1de6862c7070e15517e8792f9cfb2c25
Anna Pika <bras@vp.pl>
2020-06-05 09:58:23 +0200

    pierwsze pliki portfoliolab

--- oddamapp/models.py
+++ oddamapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Gift(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
